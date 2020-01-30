833335415f7e1949081b1fbbc297ef01f7667499
Simon <simonsch.tz@gmail.com>
2020-01-30 15:36:39 +0100

    Finish working javascript

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 (() => {
-    console.log(`Boom!`);
+    const inputs = [...document.querySelectorAll("input:not(:last-child)")];
+    const results = [...document.querySelectorAll("input:last-child")];
 
-    const addition = () => {
-        //get.add-one & .add-two
-        //calculate them
-        //display result in .add-result
+
+    const calc = () => {
+        const addOne = parseFloat(inputs[0].value);
+        const addTwo = parseFloat(inputs[1].value);
+        results[0].value = addOne + addTwo;
+        const subOne = parseFloat(inputs[2].value);
+        const subTwo = parseFloat(inputs[3].value);
+        results[1].value = subOne - subTwo;
+        const multiOne = parseFloat(inputs[4].value);
+        const multiTwo = parseFloat(inputs[5].value);
+        results[2].value = multiOne * multiTwo;
+        const divOne = parseFloat(inputs[6].value);
+        const divTwo = parseFloat(inputs[7].value);
+        results[3].value = divOne / divTwo;
+        // console.log(resultAdd);
 
     }
-
+    inputs.forEach(array => {
+        array.addEventListener("change", calc);
+    })
+    // resultAddition.addEventListener("change", addition);
 })();
