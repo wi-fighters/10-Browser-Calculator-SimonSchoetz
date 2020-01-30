c4aa170e5ca2f926794acdcde1825fa438c12106
Simon <simonsch.tz@gmail.com>
2020-01-30 14:00:10 +0100

    Write pseudio code for first calculation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 (() => {
     console.log(`Boom!`);
+
+    const addition = () => {
+        //get.add-one & .add-two
+        //calculate them
+        //display result in .add-result
+
+    }
+
 })();
